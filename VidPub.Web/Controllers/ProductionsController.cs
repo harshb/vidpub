f9ac3eae8b8b032a66347f73c110ea3b0541547d
Harsh Bhasin <hersh_b@yahoo.com>
2011-10-16 09:37:14 -0500

    end part 4

--- VidPub.Web/Controllers/ProductionsController.cs
+++ VidPub.Web/Controllers/ProductionsController.cs
@@ -1,111 +1,17 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VidPub.Web.Model;
-
-namespace VidPub.Web.Controllers
-{
-    public class ProductionsController : Controller
-    {
-
-        Productions _table;
-
-        public ProductionsController()
-        {
+using VidPub.Web.Infrastructure;
+namespace VidPub.Web.Controllers{
+    public class ProductionsController : CruddyController {
+        public ProductionsController(ITokenHandler tokenStore):base(tokenStore) {
             _table = new Productions();
-        }
-
-        public ActionResult Index()
-        {
-            return View(_table.All());
-        }
-
-        //
-        // GET: /Productions/Details/5
-
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        //
-        // GET: /Productions/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        } 
-
-        //
-        // POST: /Productions/Create
-
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-        
-        //
-        // GET: /Productions/Edit/5
- 
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Productions/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
- 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Productions/Delete/5
- 
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Productions/Delete/5
-
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
- 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            ViewBag.Table = _table;
         }
     }
 }
+
---
+++ VidPub.Web/Controllers/ProductionsControllerOld.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidPub.Web.Model;
+
+
+namespace VidPub.Web.Controllers
+{
+    public class ProductionsControllerOld : Controller
+    {
+
+        Productions _table;
+
+        public ProductionsControllerOld()
+        {
+            _table = new Productions();
+        }
+
+        public ActionResult Index()
+        {
+            return View(_table.All());
+            
+        }
+
+        //
+        // GET: /Productions/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Productions/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Productions/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            dynamic item = _table.CreateFrom(collection);
+            try
+            {
+               
+                _table.Insert(item);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                TempData["alert"] = "There was an error adding this item";
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Productions/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Productions/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            var model = _table.CreateFrom(collection);
+            try
+            {
+                _table.Update(model, id);
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception x)
+            {
+
+                TempData["alert"] = "There was an error adding this item";
+                return View(model);
+            }
+        }
+
+      
+
+
+        //
+        // GET: /Productions/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Productions/Delete/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                _table.Delete(id);
+              
+            }
+            catch
+            {
+                TempData["alert"] = "There was an error deleting this item";
+                
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
